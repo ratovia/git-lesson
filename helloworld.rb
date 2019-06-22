dc0155b0c140ae9dcdc9baf7ee25507e194e93b2
hidemaro nakano <vita.hima-hima-0427@ezweb.ne.jp>
2019-06-22 12:39:09 +0900

    コンフリ

--- helloworld.rb
+++ helloworld.rb
@@ -1,2 +1,10 @@
-hello word
-hello  hello
+d
+jmyhtefihllo
+riudkl;fhjkdwelq;jfihejdkqlwfjjdkl;wjfhlkjjdlwjfhjbdmklqkjhligkjbkn
+dwjkckdk.sal/;
+c jsfml;w:d
+lwdfkck;lwd:s
+wdihdcl;q:dlcm
+cwjikld;s/x
+feklc
+
